abec331a13b581019bfd3988f002681bed65ce1e
paulos <vanana@10>
2015-02-25 21:48:59 -0300

    example 1

--- js/google-maps.js
+++ js/google-maps.js
@@ -7,12 +7,21 @@ var myOptions = {
 };
 var mapa = new google.maps.Map(document.getElementById("mapa"), myOptions);
 
+function toNumber(x) {
+    return parseFloat(x.replace('.', '').replace(',', '.'));
+}
+
+var escala = d3.scale
+    .quantile()
+    .range(d3.range(5).map(d3.scale.linear().range(['red', 'yellow']).domain([0, 4])))
+    .domain(_.map(dados.poligonos, 'valor'));
+
 _.each(dados.poligonos, function(g, i) {
     var range = dados.ranges[g.range];
-    var gp = new google.maps.Polyline({
-        paths: geometria[g.md5].split(' ').map(google.maps.geometry.encoding.decodePath),
-        strokeColor: '#' + range.fill_color,
-        fillColor: '#ffffff',
+    var gp = new google.maps.Polygon({
+        paths: (geometria[g.md5]+' ').split(' ').map(google.maps.geometry.encoding.decodePath),
+        strokeColor: 'red',
+        fillColor: escala(g.valor),
         fillOpacity: 1,
         strokeOpacity: 1,
         strokeWeight: 1,
